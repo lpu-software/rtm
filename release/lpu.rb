553a17f482f4756ae5deeab954abc8b94ac59336
Yatish Kumar <yatish0155@gmail.com>
2026-08-26 04:35:00 +0530

    Automate Mac and Windows release without manual drag-and-drop

--- release/lpu.rb
+++ release/lpu.rb
@@ -1,9 +1,9 @@
 class Lpu < Formula
   desc "LPU - Remote Terminal Management"
   homepage "https://github.com/lpu-software/rtm"
-  url "https://github.com/lpu-software/rtm/releases/download/v1.0.6/lpu-mac.tar.gz"
-  sha256 "fe5f6c68614a54a287352b35dbb9b67b6e0a56cfc855abb996768e8f219f4b76"
-  version "1.0.6"
+  url "https://github.com/lpu-software/rtm/releases/download/v1.0.7/lpu-mac.tar.gz"
+  sha256 "37e6b59fec629766e8d165b8818f647339f2f20c6a2cc773279b81d7afc1a03d"
+  version "1.0.7"
 
   def install
     # Install the binary
