fc0d19364703ab3acec453c9489ae4ef583d7070
Yatish Kumar <yatish0155@gmail.com>
2026-09-05 23:46:03 +0530

    chore: release v1.0.22 with macOS screen recording fix

--- release/lpu.rb
+++ release/lpu.rb
@@ -1,9 +1,9 @@
 class Lpu < Formula
   desc "LPU - Remote Terminal Management"
   homepage "https://github.com/lpu-software/rtm"
-  url "https://github.com/lpu-software/rtm/releases/download/v1.0.21/lpu-mac.tar.gz"
-  sha256 "2c176b0d01a782b8ed2d322e78a8bbc75158ca8fc6d47123b7130120bdab0a59"
-  version "1.0.21"
+  url "https://github.com/lpu-software/rtm/releases/download/v1.0.22/lpu-mac.tar.gz"
+  sha256 "b7852c9ff712cd07511115f3a58c747b526eda9b24d35e26dc7dee9ab90de0d7"
+  version "1.0.22"
 
   def install
     # Install the binary
